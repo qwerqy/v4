aa696651e5ed8ac21e950a2a9d21af08d0eaef32
Amin Roslan <amnrsln@gmail.com>
2022-07-14 17:54:18 +0800

    Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,12 @@ import {
   VscSync,
   VscError,
   VscWarning,
+  VscBell,
 } from "react-icons/vsc";
 
 const Footer = () => {
   return (
-    <div className="bg-fill text-fillText rounded-b-xl">
+    <div className="bg-fill text-fillText rounded-b-xl flex justify-between">
       <div className="flex flex-row py-1 px-6 gap-4 items-center">
         <div className="flex justify-center items-center gap-1">
           <VscSourceControl />
@@ -21,6 +22,29 @@ const Footer = () => {
         <div className="flex justify-center items-center gap-1">
           <VscWarning />
           <span>0</span>
+        </div>
+      </div>
+      <div className="flex flex-row py-1 px-6 gap-4 items-center">
+        <div>
+          <span>Ln 1, Col 1</span>
+        </div>
+        <div>
+          <span>Spaces: 2</span>
+        </div>
+        <div>
+          <span>UTF-8</span>
+        </div>
+        <div>
+          <span>LF</span>
+        </div>
+        <div>
+          <span>Typescript</span>
+        </div>
+        <div>
+          <span>Prettier</span>
+        </div>
+        <div className="flex justify-center items-center gap-1">
+          <VscBell />
         </div>
       </div>
     </div>
